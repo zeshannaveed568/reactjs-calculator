4f4be00e350b5177f16d3eb7f70207ddb13d0bde
Zeshan Naveed <zeshannaveed568@gmail.com>
2023-02-16 23:08:38 +0500

    reactjs-Calculator

---
+++ src/components/Calculator/index.js
@@ -0,0 +1,87 @@
+import React, { useState } from "react";
+import { Button, Container, Current, Previous, Screen } from "./Styled";
+
+export default function Calculator() {
+  const [previous, setPrevious] = useState("");
+  const [current, setCurrent] = useState("");
+  const [operation, setOperation] = useState("");
+
+  const appendValue = (e) => {
+    const value = e.target.getAttribute("data");
+
+    if (value === "." && current.includes(".")) {
+      return;
+    }
+    setCurrent(current + value);
+    console.log(value);
+  };
+
+  const handleDelete = () => {
+    setCurrent(String(current).slice(0, -1));
+  };
+
+  const handleAllClear = () => {
+    setCurrent("");
+    setPrevious("");
+    setOperation("");
+  };
+
+  return (
+    <Container>
+      <Screen>
+        <Previous>
+          {previous} {operation}
+        </Previous>
+        <Current>{current}</Current>
+      </Screen>
+
+      <Button onClick={handleAllClear} gridSpan={2} controls>
+        AC{" "}
+      </Button>
+      <Button controls onClick={handleDelete}>
+        {" "}
+        DEL
+      </Button>
+      <Button operation>÷</Button>
+      <Button data={"7"} onClick={appendValue}>
+        7
+      </Button>
+      <Button data={"8"} onClick={appendValue}>
+        8
+      </Button>
+      <Button data={"9"} onClick={appendValue}>
+        9
+      </Button>
+      <Button operation>x</Button>
+      <Button data={"4"} onClick={appendValue}>
+        4
+      </Button>
+      <Button data={"5"} onClick={appendValue}>
+        5
+      </Button>
+      <Button data={"6"} onClick={appendValue}>
+        6
+      </Button>
+      <Button operation>+</Button>
+      <Button data={"1"} onClick={appendValue}>
+        1
+      </Button>
+      <Button data={"2"} onClick={appendValue}>
+        2
+      </Button>
+      <Button data={"3"} onClick={appendValue}>
+        3
+      </Button>
+      <Button operation>-</Button>
+      <Button period data={"."} onClick={appendValue}>
+        .
+      </Button>
+      <Button data={"0"} onClick={appendValue}>
+        0{" "}
+      </Button>
+      <Button equals gridSpan={2}>
+        =
+      </Button>
+    </Container>
+  );
+}
